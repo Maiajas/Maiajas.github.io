8ba45a7872a9eed6a7aa72faa46aaadea98419d8
Maiajas <84944139+Maiajas@users.noreply.github.com>
2023-08-09 18:49:33 +0000

    test

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js', { scope: '/' });
 }
 function toggleVisibility(id) { 
-    if(document.getElementById(id).style.display == 'none'){
+    if(document.getElementById(id).style.display != 'block'){
         document.getElementById(id).style.display = 'block';
-    }else if(document.getElementById(id).style.display == 'block'){
+    }else{
         document.getElementById(id).style.display = 'none'; 
     }
 } 
