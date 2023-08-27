5f0c34799710035e8639c5ae1240cd67ff90f810
Maiajas <84944139+Maiajas@users.noreply.github.com>
2023-08-27 16:23:08 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 "use strict";
 import { firstLoad } from "./modules/func.mjs";
 firstLoad();
+window.addEventListener('load', () => {registerSW();});
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('/src/serviceworker.js');
+        }
+        catch (e) {
+            console.log('SW registration failed');
+        }
+    }
+}
